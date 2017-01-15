499fda9f8a4f34b638de43dfa6f037eaeae501bc
Luka Cvetkovic <cvetko911@hotmail.com>
2017-01-15 11:53:39 +0100

    Add project files.

---
+++ SignalRChat/Common/MessageDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChat.Common
+{
+    public class MessageDetail
+    {
+
+        public string UserName { get; set; }
+
+        public string Message { get; set; }
+
+        public string Color { get; set; }
+
+    }
+}
